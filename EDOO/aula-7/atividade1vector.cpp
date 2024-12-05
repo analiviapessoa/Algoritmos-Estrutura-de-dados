eed254a344d3f9a6bf4c88c0570a2dda56f90b23
Ana Lívia da Costa Pessoa <analiviapessoa@uol.com.br>
2024-12-04 21:57:47 -0300

    atividades

--- EDOO/aula-7/atividade1vector.cpp
+++ EDOO/aula-7/atividade1vector.cpp
@@ -1,3 +1,10 @@
+/*Atividade: Reverter um vetor de inteiros
+- Lê do teclado o tamanho do array (N <= 100)
+- Cria um array aleatório em ordem crescente com valores entre 0 e 1000 (usar função rand())
+- Imprime o array original
+- Inverte o array
+- Imprime o array invertido*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -26,10 +33,3 @@ int main(){
 
     return 0;
 }
-
-/*Atividade: Reverter um vetor de inteiros
-- Lê do teclado o tamanho do array (N <= 100)
-- Cria um array aleatório em ordem crescente com valores entre 0 e 1000 (usar função rand())
-- Imprime o array original
-- Inverte o array
-- Imprime o array invertido*/
--- EDOO/aula-7/atividade2.cpp
+++ EDOO/aula-7/atividade2.cpp
@@ -1,37 +1,59 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main(){
-
-    int M, N, P;
-    while (M <= 2 || M >= 100){
-        cin >> M;
-    }
-
-    while (N <= 2 || N >= 100){
-        cin >> N;
-    }
-
-    while (P <= 2 || P >= 100){
-        cin >> P;
-    }
-
-    int matrizA[M][N];
-
-    for (int i = 0 ; i < M ; i++){
-        for (int j = 0 ; j < N ; j++){
-            cin >> matrizA[i][j]; 
-        }
-    }
-
-
-    int matrizB[N][P];
-
-    return 0;
-}
-
 /*Atividade: Calcular o produto de duas matrizes
 Lê três inteiros positivos M N P no intervalo (2, 10)
 Cria duas matrizes aleatórias A e B de dimensões MxN e NxP com valores no intervalo (-100, +100)
 Calcula a matriz produto C = A*B de dimensões MxP
 Imprime a matrizes A, B, C*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int M=0, N=0, P=0;
+
+    while(M<2 || M>10){
+        cin >> M;
+    }
+
+    while(N<2 || N>10){
+        cin >> N;
+    }
+
+    while(P<2 || P>10){
+        cin >> P;
+    }
+
+    int matrizA[M][N], matrizB[N][P];
+
+    for (int i = 0 ; i < M ; i++){
+        for (int j = 0 ; j < N ; j++){
+            matrizA[i][j] = rand()%101;
+        }
+    }
+
+    for (int i = 0 ; i < N ; i++){
+        for (int j = 0 ; j < P ; j++){
+            matrizB[i][j] = rand()%101;
+        }
+    }
+
+    cout <<"Matriz A: " << endl;
+    for (int i = 0 ; i < M ; i++){
+        for (int j = 0 ; j < N ; j++){
+            cout << matrizA[i][j] << " "; 
+        }
+        cout << endl;
+    }
+    cout << endl;
+
+    cout <<"Matriz B: " << endl;
+    for (int i = 0 ; i < N ; i++){
+        for (int j = 0 ; j < P ; j++){
+            cout << matrizB[i][j] << " "; 
+        }
+        cout << endl;
+    }
+    cout << endl;
+
+    return 0;
+}
